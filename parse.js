62bf975d5f7f52179556aaca8e5018668416c242
Paul Shen <paulshen0@gmail.com>
2017-04-07 14:23:52 -0700

    parse to Data.js

--- parse.js
+++ parse.js
@@ -1,6 +1,7 @@
 import fs from 'fs';
 
 fs.readdir(`${__dirname}/docs`, function(err, files) {
+  let data = {};
   files.forEach(function(file, i) {
     let f = require(`${__dirname}/docs/${file}`);
     if (f.content && f.content[0] === '{') {
@@ -8,11 +9,18 @@ fs.readdir(`${__dirname}/docs`, function(err, files) {
       switch (d.type) {
         case 'api':
           console.log(d.name);
+          data[d.name] = d;
           break;
         case 'component':
           console.log(`<${d.componentName}>`);
+          data[d.componentName] = d;
           break;
       }
     }
   });
+  fs.writeFile(`${__dirname}/site/src/Data.js`, `module.exports=${JSON.stringify(data)};`, function(err) {
+    if (!err) {
+      console.log('done');
+    }
+  })
 });
